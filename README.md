e9ca0723d0a287b34b09cdeba9f8ca4d4b91a820
Bruno Gonçalves Ferreira <brunogoncalvesferreira@outlook.com>
2023-11-12 20:01:56 -0300

    :tada: create project open_ai

---
+++ README.md
@@ -0,0 +1,19 @@
+# Projetinho OpenAI
+
+Construí uma aplicação simples usando HTML, TailwindCSS e JavaScript, onde faço perguntas e recebo respostas.
+
+<br>
+
+## Tecnologias
+
+Abaixo as tecnologias usadas no proejto:
+
+- HTML
+- Tailwindcss
+- Javascript
+- API OPEN AI
+
+<br>
+
+<img src=".github/preview2.png">
+<img src=".github/preview1.png">
---
+++ main.js
@@ -0,0 +1,56 @@
+const question = document.getElementById('question');
+const answer = document.getElementById('answer');
+const human = document.getElementById('human');
+const ai = document.getElementById('ai');
+const chat = document.getElementById('chat');
+const button = document.querySelector('button');
+
+button.addEventListener('click', getCompletions);
+
+const OPEN_AI_KEY = ""
+
+async function getCompletions(event) {
+    event.preventDefault()
+
+    const questionValue = question.value;
+
+    try {
+        const response = await fetch('https://api.openai.com/v1/completions', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${OPEN_AI_KEY}`
+        },
+        body: JSON.stringify(
+            {
+                model: 'text-davinci-003',
+                prompt: questionValue,
+                max_tokens: 100,
+                temperature: 0.4
+            }
+        )
+    })
+    const data = await response.json();
+
+    chat.innerHTML = `
+        <p class="text-slate-400 leading-relaxed" id="human">
+            <span class="block text-blue-500 font-bold text-lg mr-2">Eu:</span>
+            ${questionValue}
+        </p>
+        <p class="text-slate-400 leading-relaxed" id="ai">
+            <span span class="block text-green-500 font-bold text-lg mr-2">Chat GPT:</span>
+            ${data.choices[0].text}
+        </p>
+    `
+
+    question.value = ''
+    question.focus()
+
+    } catch (error) {
+        
+        console.log(error);
+    }
+    
+
+    
+}
